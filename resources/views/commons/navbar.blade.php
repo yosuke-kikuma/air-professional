81faf46632c5ddd7f2b6e4118754c73b08612267
yosuke.kikuma <yosuke19910412@gmail.com>
2021-05-28 02:45:22 +0000

    0528_modify

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,23 +1,20 @@
 <header>
-    <nav class="navbar navbar-expand-sm bg-light">
-        <div class="head_left">
+    <nav class="navbar navbar-expand-sm bg-light d-flex justify-content-between">
+        <div class="navbar_left">
         <p>日系の航空業界専門サイト</p>
             <div class="foot_left">
             <a class="navbar-brand" href="/">AIR Professional</a>
             </div>
         </div>
-
-        <div class="nav_right collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
+        
+        <div class="navbar_right">
             @if (Auth::check())
-                <li class="nav_item">{!! link_to_route('users.favorites', 'お気に入り', ['id' => Auth::id()]) !!}</li>
-                <li class="nav_item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                <div class="nav_item">{!! link_to_route('users.favorites', 'お気に入り', ['id' => Auth::id()]) !!}</div>
+                <div class="nav_item">{!! link_to_route('logout.get', 'ログアウト') !!}</div>
             @else
-                <li class="nav_item">{!! link_to_route('signup.get', '会員登録', [], ['class' => 'nav-link']) !!}</li>
-                <li class="nav_item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                <div class="nav_item">{!! link_to_route('signup.get', '会員登録', [], ['class' => 'nav-link']) !!}</div>
+                <div class="nav_item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</div>
             @endif
-            </ul>
         </div>
     </nav>
 </header>
